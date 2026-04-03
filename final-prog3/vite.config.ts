af5e651e621ab81c8388d6e838a9c9296fcaf79e
Joel <joeldario9912@gmail.com>
2026-04-02 21:41:10 -0300

    front fix: vite context and configs

--- final-prog3/vite.config.ts
+++ final-prog3/vite.config.ts
@@ -1,16 +1,19 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
+import { resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const root = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
         //d:aplicaion/dist/
-        index: resolve(__dirname, "index.html"),
-        login: resolve(__dirname, "src/pages/auth/login/login.html"),
-        registro: resolve(__dirname, "src/pages/auth/registro/registro.html"),
-        adminHome: resolve(__dirname, "src/pages/admin/home/home.html"),
-        clientHome: resolve(__dirname, "src/pages/client/home/home.html"),
+        index: resolve(root, "index.html"),
+        login: resolve(root, "src/pages/auth/login/login.html"),
+        registro: resolve(root, "src/pages/auth/registro/registro.html"),
+        adminHome: resolve(root, "src/pages/admin/home/home.html"),
+        clientHome: resolve(root, "src/pages/client/home/home.html"),
       },
     },
   },
