878369b7759fa1bc8d57fb1d52457da5ceba3fad
Joel <joeldario9912@gmail.com>
2026-05-23 11:45:07 -0300

    chore: delete final-prog3 directorie

--- final-prog3/src/types/IUserSession.ts
+++
@@ -1,7 +0,0 @@
-import type { Rol } from "./Rol";
-
-export interface IUserSession {
-  email: string;
-  loggedIn: boolean;
-  role: Rol;
-}
--- final-prog3/src/utils/const.ts
+++
@@ -1,3 +0,0 @@
-export const loginPath: string = "/src/pages/auth/login/login.html";
-export const clientPath: string = "/src/pages/client/home/home.html";
-export const adminPath: string = "/src/pages/admin/home/home.html";
